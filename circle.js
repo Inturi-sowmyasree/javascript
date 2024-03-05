3968bffdbe8c877bf73a32410a499d3d8634a644
Inturi-sowmyasree <inturisowmyasree51@gmail.com>
2024-03-05 20:00:23 +0530

    changes some function

---
+++ circle.js
@@ -0,0 +1,13 @@
+function circle() {
+    const radius = 5;
+    const pi = Math.PI;
+
+    const area = pi * radius * radius;
+    const perimeter = 2 * pi * radius;
+
+    console.log(area);
+    console.log(perimeter);
+}
+
+circle();
+ // without parameter without return type 
